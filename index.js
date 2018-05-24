5fb5dca93643a5ff281eec9c8775ecb8a087d363
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-05-24 13:57:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,16 +70,16 @@ function createRock(x) {
  
  
 function endGame() {
-  while(ROCKS.length > 0){
-    GAME.removeChild(ROCKS[0]);
-    ROCKS.shift();
-  }
+  clearInterval(gameInterval)
 
-  clearInterval(gameInterval);
-  window.removeEventListener('keydown', moveDodger);
-  alert("YOU LOSE!");
+  ROCKS.forEach(function(rock) { rock.remove() })
+
+  window.removeEventListener('keydown', moveDodger)
+
   START.innerHTML = 'Play again?'
   START.style.display = 'inline'
+
+  return alert('YOU LOSE!')
 }
 
 
